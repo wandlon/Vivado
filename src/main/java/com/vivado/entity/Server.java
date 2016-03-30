e82077cdbfddb0474d6791f53fba6ca2e7efe43a
wandlon <845137413@qq.com>
2016-03-30 23:28:52 +0800

    修改配置文件

---
+++ src/main/java/com/vivado/entity/Server.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.vivado.entity;
+
+/**
+ * 虚拟主机实体类
+ * @author 德龙
+ *
+ */
+public class Server {
+
+}
---
+++ src/main/java/com/vivado/openstack/ServerHandler.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.vivado.openstack;
+
+/**
+ * 虚拟主机操作类
+ * @author 德龙
+ *
+ */
+public class ServerHandler {
+
+}
